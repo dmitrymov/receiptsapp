8cf35d3bde11eae98dbd0e33a31357e7b2b3ea7e
dimmov21 <dimmov21@gmail.com>
2025-04-20 19:27:03 +0000

    database store

---
+++ lib/database_helper.dart
@@ -0,0 +1,71 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+class DatabaseHelper {
+  static Database? _database;
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await _initDatabase();
+    return _database!;
+  }
+
+  Future<Database> _initDatabase() async {
+    final databasePath = await getDatabasesPath();
+    final path = join(databasePath, 'recipe_app.db');
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: _onCreate,
+    );
+  }
+
+  Future<void> _onCreate(Database db, int version) async {
+    await db.execute('''
+      CREATE TABLE recipes (
+        id TEXT PRIMARY KEY,
+        name TEXT,
+        ingredients TEXT,
+        instructions TEXT,
+        category TEXT
+      )
+    ''');
+    await db.execute('''
+      CREATE TABLE categories (
+        id TEXT PRIMARY KEY,
+        name TEXT
+      )
+    ''');
+  }
+
+  Future<int> insertRecipe(Map<String, dynamic> recipe) async {
+    final db = await database;
+    return await db.insert('recipes', recipe);
+  }
+
+  Future<List<Map<String, dynamic>>> getRecipes() async {
+    final db = await database;
+    return await db.query('recipes');
+  }
+
+  Future<int> updateRecipe(Map<String, dynamic> recipe) async {
+    final db = await database;
+    return await db.update(
+      'recipes',
+      recipe,
+      where: 'id = ?',
+      whereArgs: [recipe['id']],
+    );
+  }
+
+  Future<int> deleteRecipe(String id) async {
+    final db = await database;
+    return await db.delete(
+      'recipes',
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+  }
+}
+
+
